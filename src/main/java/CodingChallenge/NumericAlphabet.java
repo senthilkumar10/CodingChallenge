5780f034d22f5eba52476265543dabd406df31ca
senthil <senthilkumar10@gmail.com>
2020-05-04 09:29:57 +0200

    NumericAlphabet

---
+++ src/main/java/CodingChallenge/NumericAlphabet.java
@@ -0,0 +1,49 @@
+package CodingChallenge;
+
+public class NumericAlphabet {
+
+
+    public static void main(String[] args) {
+
+        int inputNumber = 100;
+
+        if(inputNumber>0){
+
+            if(inputNumber<=26){
+
+                System.out.println(sendAlphabet(inputNumber));
+
+            }else{
+                StringBuilder finalAlphabets = new StringBuilder();
+                int numberOfAlphabets = inputNumber/26;
+
+                for(int i=0;i<numberOfAlphabets;i++){
+                    finalAlphabets.append("Z");
+                }
+
+                int remainingDigits = inputNumber-(26*numberOfAlphabets);
+
+                finalAlphabets.append(sendAlphabet(remainingDigits));
+
+                System.out.println(finalAlphabets);
+
+
+            }
+
+        }else{
+            System.out.println("The input number be should greater than 0");
+        }
+
+    }
+
+
+    private static String sendAlphabet(int number){
+        String[] alphabet = {"A", "B", "C", "D", "E", "F", "G","H","I","J","K","L","M","N","O",
+                "P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+        return alphabet[number-1];
+    }
+
+
+
+}
